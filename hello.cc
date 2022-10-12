482d1bcf0c3cb4fc07eb3cefd0ea7f8fabac7c3c
Jed Helmers <jedhelmers@gmail.com>
2022-10-11 20:34:56 -0400

    initial commit

---
+++ hello.cc
@@ -0,0 +1,16 @@
+#include <napi.h>
+#include <iostream>
+
+Napi::String Method(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  std::cout << &info << "\n" << env;
+  return Napi::String::New(env, "world");
+}
+
+Napi::Object Init(Napi::Env env, Napi::Object exports) {
+  exports.Set(Napi::String::New(env, "hello"),
+              Napi::Function::New(env, Method));
+  return exports;
+}
+
+NODE_API_MODULE(hello, Init)
